dbdcb11023fbf215c9ff7ff9d616a27f3b70a0bc
loses <loses626@gmail.com>
2023-01-27 17:50:47 +0600

    Начал

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        String string = "the quick brown fox jumps over the lazy dog";
+
+    }
+}
